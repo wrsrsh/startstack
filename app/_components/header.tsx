71d6123964dc804555134addb5d7493b114047ef
Waris <warisareshi@gmail.com>
2025-01-01 04:57:33 +0530

    fix: redundant trycatch removed from middleware

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,8 +1,5 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { HeaderButton } from "./header-button";
-import Image from "next/image";
 import { GithubIcon, LogInIcon } from "lucide-react";
 import { ModeToggle } from "@/components/mode-toggle";
 
@@ -10,7 +7,6 @@ export function Header() {
   return (
     <section className="flex w-screen items-center justify-between border-b border-border px-10 py-3.5 sm:px-20 md:px-32 lg:px-40">
       <Link className="flex items-center gap-2 font-medium" href={"/"}>
-        {/* <Image className="h-5 w-5" src={"/logo.png"} alt="logo" /> */}
         Startstack
       </Link>
       <div className="flex items-center gap-2">
--- middleware.ts
+++ middleware.ts
@@ -1,41 +1,32 @@
 import { betterFetch } from "@better-fetch/fetch";
-import type { auth } from "@/lib/auth/server";
+import { Session } from "better-auth";
 import { NextResponse, type NextRequest } from "next/server";
 
-type Session = typeof auth.$Infer.Session;
-
 export default async function authMiddleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
   const isAuthPage =
-    pathname.startsWith("/login") || pathname.startsWith("/signup");
-  const isAppPage = pathname.startsWith("/app/");
+    request.nextUrl.pathname.startsWith("/login") ||
+    request.nextUrl.pathname.startsWith("/signup");
+  const isAppPage = request.nextUrl.pathname.startsWith("/app/");
 
-  try {
-    const { data: session } = await betterFetch<Session>(
-      "/api/auth/get-session",
-      {
-        baseURL: request.nextUrl.origin,
-        headers: {
-          cookie: request.headers.get("cookie") || "",
-        },
+  const { data: session } = await betterFetch<Session>(
+    "/api/auth/get-session",
+    {
+      baseURL: request.nextUrl.origin,
+      headers: {
+        cookie: request.headers.get("cookie") || "",
       },
-    );
+    },
+  );
 
-    if (session && isAuthPage) {
-      return NextResponse.redirect(new URL("/app/home", request.url));
-    }
-
-    if (!session && isAppPage) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-
-    return NextResponse.next();
-  } catch (error) {
-    if (isAppPage) {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-    return NextResponse.next();
+  if (session && isAuthPage) {
+    return NextResponse.redirect(new URL("/app/home", request.url));
   }
+
+  if (!session && isAppPage) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
